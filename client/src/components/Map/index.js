cb908d3669bc754cd4ed7f9aeb80f701be3a3c26
Your Name <you@example.com>
2021-05-08 16:21:45 -0600

    add Map component to project and some styling

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+import "./map.css";
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '50vh',
+    latitude: 37.6,
+    longitude: -95.665,
+    zoom: 2
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/mapbox/dark-v10"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
+  );
+}
+
+export default Map;
