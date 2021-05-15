551e348ce07cb8f088d1641d4d0d71f1898de3c3
kayvonk <kayvonk@gmail.com>
2021-05-15 08:01:28 -0600

    Added FormCard component to separate styling overlap with Container component

--- client/src/components/FormCard/index.js
+++ client/src/components/FormCard/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
-  return (
-    <div>
-
-    </div>
-  );
+  return <div className={`card container${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
+
 
 export default Card;
