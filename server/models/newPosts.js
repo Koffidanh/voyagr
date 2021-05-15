fca742a327eaebee2b8ce24d8489f8f532210460
Your Name <you@example.com>
2021-05-15 11:10:32 -0600

    add map-pin, status.js and index.js to project

--- server/models/newPosts.js
+++ server/models/newPosts.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const { Schema }  = mongoose;
 
+const requiredNumber = {
+  type: Number,
+  required: true,
+};
+
 const newPostsSchema = new Schema({
     title: {
       type: String,
@@ -15,16 +20,17 @@ const newPostsSchema = new Schema({
       max: 10,
       default: 0,
     },
-    // latitude: {
-    //   ...requiredNumber,
-    //   min: -90,
-    //   max: 90,
-    // },
-    // longitude: {
-    //   ...requiredNumber,
-    //   min: -180,
-    //   max: 180,
-    // },
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
+    timestamp: true, 
   });
   
   const newPosts = mongoose.model('newPosts', newPostsSchema);
