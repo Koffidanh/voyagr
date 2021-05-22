8942d5e9c2a59730639d830c1a54bd67108fd7fb
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-22 10:37:46 -0600

    Moved the geocoder to the nav, and finished the post routes for the PostForm component

--- client/src/Map.js
+++ client/src/Map.js
@@ -3,17 +3,19 @@ import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import ReactMapGL, { Marker, Popup, GeolocateControl } from 'react-map-gl';
 import Geocoder from 'react-map-gl-geocoder';
-// import MapGL from 'react-map-gl'
+import { Nav, NavLink, NavMenu } from "./NavbarElements"
+import BurgerMenu from "./components/Dropdown"
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { listNewPosts } from './API';
-import NewPostForm from './components/PostForm/NewPostForm';
 import "./map.css";
+import { API } from "./utils/API"
+import { useAuth0 } from '@auth0/auth0-react';
+import ProfileImage from './components/ProfileImage';
 
-// const geolocateControlStyle = {
-//   right: 10,
-//   top: 10
-// };
-
-const Map = () => {
+export const Header = () => {
   const [newPosts, setNewPosts] = useState([]);
   const [showPopup, setShowPopup] = useState({});
   const [addPostLocation, setAddPostLocation] = useState(null);
@@ -42,11 +44,9 @@ const Map = () => {
 
   const showAddMarkerPopup = (event) => {
     const [longitude, latitude] = event.lngLat;
-    setAddPostLocation({
-      latitude,
-      longitude,
-    });
+    setAddPostLocation({ latitude, longitude });
   };
+
   const handleGeocoderViewportChange = useCallback(
     (newViewport) => {
       const geocoderDefaultOverrides = { transitionDuration: 1000 };
@@ -58,128 +58,210 @@ const Map = () => {
     []
   );
 
-  return (
-    <ReactMapGL
-      ref={mapRef}
-      {...viewport}
-      mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={setViewport}
-      onDblClick={showAddMarkerPopup}
-    >
-      {/* <GeolocateControl
+  const timestamp = Date.now();
+  const time = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(timestamp)
+  const { user } = useAuth0();
+  const { picture, sub } = user;
+  const userID = sub;
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+    image: "",
+    latitude: "",
+    longitude: "",
+    visitDate: "",
+    userID: "",
+    timestamp: ""
+  });
 
+  function handleChange(event) {
+
+    const { name, value } = event.target
+    setInput({ ...input, [event.target.name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput({ title: "", description: "", image: "", visitDate: "" })
+
+    const newPost = {
+      title: input.title,
+      description: input.description,
+      image: input.image,
+      latitude: addPostLocation.latitude,
+      longitude: addPostLocation.longitude,
+      visitDate: input.visitDate,
+      userID: userID,
+      timestamp: time
+    }
+    console.log(newPost);
+    API.savePost(newPost).catch(e => console.log(e))
+  }
+
+
+
+  return (
+    <>
+      <Nav>
+        <div
+          ref={geocoderContainerRef}
+          style={{ position: "relative", left: 20 }}
+        />
+        <NavMenu>
+          <NavLink to="/dashboard" activeStyle={{ textDecoration: "none", color: "#3e81c9" }}>
+            <FontAwesomeIcon icon={faHome} size="lg" />
+          </NavLink>
+          <NavLink to="/friends" activeStyle={{ textDecoration: "none", color: "#3e81c9" }}>
+            <FontAwesomeIcon icon={faUserFriends} size="lg" />
+          </NavLink>
+          <NavLink to="/settings" activeStyle={{ textDecoration: "none", color: "#3e81c9" }}>
+            <FontAwesomeIcon icon={faCog} size="lg" />
+          </NavLink>
+        </NavMenu>
+        <BurgerMenu />
+      </Nav>
+      <ReactMapGL
+        style={{ top: 1 }}
+        ref={mapRef}
+        {...viewport}
+        mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={setViewport}
+        onClick={showAddMarkerPopup}
+        transitionDuration="200"
+      >
+        {/* <GeolocateControl
         style={geolocateControlStyle}
         positionOptions={{ enableHighAccuracy: true }}
         trackUserLocation={true}
         auto
       /> */}
-
-      <div
-        ref={geocoderContainerRef}
-        style={{ position: "absolute", top: 20, left: 20, zIndex: 1 }}
-      />
-      <Geocoder
-        mapRef={mapRef}
-        onViewportChange={handleGeocoderViewportChange}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        position="top-left"
-      />
-
-      {
-        newPosts.map(post => (
-          <React.Fragment key={post._id}>
-            <Marker
-              latitude={post.latitude}
-              longitude={post.longitude}
-            >
-              <div
-                onClick={() => setShowPopup({
-                  // ...showPopup,
-                  [post._id]: true,
-                })}
+        <Geocoder
+          mapRef={mapRef}
+          containerRef={geocoderContainerRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          placeholder="Find Your Destination"
+        />
+        {
+          newPosts.map(post => (
+            <React.Fragment key={post._id}>
+              <Marker
+                latitude={post.latitude}
+                longitude={post.longitude}
               >
-                <svg
-                  className="marker yellow"
-                  style={{
-                    height: `${6 * viewport.zoom}px`,
-                    width: `${6 * viewport.zoom}px`,
-                  }}
-                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
-                  <g>
+                <div
+                  onClick={() => setShowPopup({
+                    // ...showPopup,
+                    [post._id]: true,
+                  })}
+                >
+                  <svg
+                    className="marker yellow"
+                    style={{
+                      height: `${6 * viewport.zoom}px`,
+                      width: `${6 * viewport.zoom}px`,
+                    }}
+                    version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
                     <g>
-                      <path d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z" />
+                      <g>
+                        <path d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z" />
+                      </g>
                     </g>
-                  </g>
-                </svg>
-              </div>
-            </Marker>
-            {
-              showPopup[post._id] ? (
-                <Popup
-                  latitude={post.latitude}
-                  longitude={post.longitude}
-                  closeButton={true}
-                  closeOnClick={false}
-                  dynamicPosition={true}
-                  onClose={() => setShowPopup({})}
-                  anchor="top" >
-                  <div className="popup">
-                    <h3>{post.title}</h3>
-                    <p>{post.comments}</p>
-                    <small>Visited on: {new Date(post.visitDate).toLocaleDateString()}</small>
-                    {post.image && <img src={post.image} alt={post.title} />}
-                  </div>
-                </Popup>
-              ) : null
-            }
-          </React.Fragment>
-        ))
-      }
-      {
-        addPostLocation ? (
-          <>
-            <Marker
-              latitude={addPostLocation.latitude}
-              longitude={addPostLocation.longitude}
-            >
-              <div>
-                <svg
-                  className="marker react-blue"
-                  style={{
-                    height: `${6 * viewport.zoom}px`,
-                    width: `${6 * viewport.zoom}px`,
-                  }}
-                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
-                  <g>
+                  </svg>
+                </div>
+              </Marker>
+              {
+                showPopup[post._id] ? (
+                  <Popup
+                    latitude={post.latitude}
+                    longitude={post.longitude}
+                    closeButton={true}
+                    closeOnClick={false}
+                    dynamicPosition={true}
+                    onClose={() => setShowPopup({})}
+                    anchor="top" >
+                    <div className="popup">
+                      <h3>{post.title}</h3>
+                      <p>{post.comments}</p>
+                      <small>Visited on: {new Date(post.visitDate).toLocaleDateString()}</small>
+                      {post.image && <img src={post.image} alt={post.title} />}
+                    </div>
+                  </Popup>
+                ) : null
+              }
+            </React.Fragment>
+          ))
+        }
+        {
+          addPostLocation ? (
+            <>
+              <Marker
+                latitude={addPostLocation.latitude}
+                longitude={addPostLocation.longitude}
+              >
+                <div>
+                  <svg
+                    className="marker react-blue"
+                    style={{
+                      height: `${6 * viewport.zoom}px`,
+                      width: `${6 * viewport.zoom}px`,
+                    }}
+                    version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
                     <g>
-                      <path d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z" />
+                      <g>
+                        <path d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z" />
+                      </g>
                     </g>
-                  </g>
-                </svg>
-              </div>
-            </Marker>
-            <Popup
-              latitude={addPostLocation.latitude}
-              longitude={addPostLocation.longitude}
-              closeButton={true}
-              closeOnClick={false}
-              dynamicPosition={true}
-              onClose={() => setAddPostLocation(null)}
-              anchor="top" >
-              <div className="popup">
-                <NewPostForm onClose={() => {
-                  setAddPostLocation(null);
-                  getPosts();
-                }} location={addPostLocation} />
-              </div>
-            </Popup>
-          </>
-        ) : null
-      }
-    </ReactMapGL>
+                  </svg>
+                </div>
+              </Marker>
+              <Popup
+                latitude={addPostLocation.latitude}
+                longitude={addPostLocation.longitude}
+                closeButton={true}
+                closeOnClick={false}
+                dynamicPosition={true}
+                onClose={() => setAddPostLocation(null)}
+                anchor="top" >
+                <div className="popup">
+                  <form className="post-form" onSubmit={handleSubmit}>
+                    <label htmlFor="title">Title</label>
+                    <input name="title" required
+                      onChange={handleChange}
+                      value={input.title} />
+                    <label htmlFor="description">Description</label>
+                    <input name="description" rows={3}
+                      value={input.description}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="image">Image</label>
+                    <input name="image"
+                      value={input.image}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="visitDate">Visit Date</label>
+                    <input name="visitDate" type="date"
+                      value={input.visitDate}
+                      onChange={handleChange}
+                    />
+                    <input type="submit" value="Submit" />
+                  </form>
+                  {/* <NewPostForm onClose={() => {
+                                        setAddPostLocation(null);
+                                        getPosts();
+                                    }} location={addPostLocation} /> */}
+                </div>
+              </Popup>
+            </>
+          ) : null
+        }
+        <ProfileImage
+          avatarImage={picture}
+        />
+      </ReactMapGL>
+    </>
   )
-};
+}
 
-
-export default Map;
+export default Header;
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./style.css";
 import Container from "../../components/Container";
 import Navbar from "../../components/Navbar";
-import Map from "../../Map"
+import Header from "../../Map"
 // import Avatar from "../../components/Avatar"
 import MessageSender from "../../components/MessageSender"
 import Feed from "../../components/Feed";
 import Post from "../../components/Post";
 import ProfileImage from "../../components/ProfileImage";
 import { useAuth0 } from '@auth0/auth0-react';
-import Profile from "../../components/Profile"
-
 
 function DashboardPage() {
   const { user } = useAuth0();
@@ -18,38 +16,27 @@ function DashboardPage() {
 
   return (
     <div>
-      <Navbar />
-      <Container style={{ margin: 0, padding: 0, zIndex: "-1000" }}>
-        <Map />
-      </Container>
-      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 10, zIndex: "1000" }}>
-        <ProfileImage
-          avatarImage={picture}
-        />
-        <Profile />
-      </Container>
-      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 5, zIndex: "1000" }}>
-      </Container>
-      <Feed>
+      <Header />
+      <Feed style={{ marginTop: 100 }} >
         <MessageSender />
         <Post
-          // profileImage={picture}
-          // description="Boop Boop"
-          // username={name}
-          // image="https://www.nps.gov/articles/images/Cover-GAAR_SeanTevebough.jpg?maxwidth=1200&autorotate=false"
-          // timestamp=""
+          profileImage={picture}
+          description="Boop Boop"
+          username={name}
+          image="https://www.nps.gov/articles/images/Cover-GAAR_SeanTevebough.jpg?maxwidth=1200&autorotate=false"
+          timestamp=""
         // profileImage="https://pbs.twimg.com/profile_images/1148822091682045952/vR2t82Hy.jpg"
         // message="Boop Boop"
         // username={username}
         // image={image}
         // timestamp={timestamp}
         />
-        {/* <Post
+        <Post
           profileImage={picture}
           message="Boop Boop"
           username={name}
           timestamp=""
-        /> */}
+        />
       </Feed>
     </div>
   );
