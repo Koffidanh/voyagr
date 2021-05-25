eb606102aa7f74f04e1e7035e1dded0fb8f4dbb2
Your Name <you@example.com>
2021-05-24 21:16:27 -0600

    perform housekeeping on client/ server folders

--- client/src/components/FormCard/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Card(props) {
-  return <div className={`card container${props.fluid ? "-fluid" : ""}`} {...props} />;
-}
-
-export default Card;
