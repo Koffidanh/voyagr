3a2b64b480dca7bcb33e4b799851b6918f311c85
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-26 16:34:17 -0600

    Fix to pins and images not displaying

--- client/src/components/NavbarSignup/index.js
+++ client/src/components/NavbarSignup/index.js
@@ -1,25 +1,41 @@
 import React from 'react'
-import { Nav, NavBtn } from "./NavbarElements"
+import { Nav, NavBtn, NavMenu } from "./NavbarElements"
 import "./style.css";
 import LoginButton from '../LoginButton';
 import SignupButton from '../SignupButton';
-
+import { Col } from '../Grid';
 export const NavbarSignup = () => {
 
     return (
         <>
             <Nav>
-                <img
-                    src="/voyagr.png"
-                    // width="30"
-                    height="60"
-                    className="voyagr-logo"
-                    alt="Voyagr logo"
-                />
-                <NavBtn >
-                    <LoginButton />
-                    <SignupButton />
-                </NavBtn>
+                <Col size="md-8">
+                    <img
+                        style={{ marginTop: 20 }}
+                        src="/voyagr.png"
+                        // width="30"
+                        height="60"
+                        className="voyagr-logo"
+                        alt="Voyagr logo"
+                    />
+                </Col>
+                <Col size="md-8">
+
+                </Col>
+
+
+
+                {/* <NavMenu>
+
+                </NavMenu> */}
+
+                <Col size="md-8">
+
+                    <NavBtn>
+                        <LoginButton />
+                        <SignupButton />
+                    </NavBtn>
+                </Col>
             </Nav>
         </>
     )
