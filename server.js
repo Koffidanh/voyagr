be8c8cd077c224bb9c8f1ebf5b1722d145ce0daa
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-27 13:03:32 -0600

    Updated get route

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.use(routes);
 // app.use(middlewares.errorHandler);
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
