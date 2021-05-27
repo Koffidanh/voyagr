15af41687dadd5e25487d53963382b5d8b2809f1
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-27 14:10:27 -0600

    Added template server files

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(routes);
 // app.use(middlewares.errorHandler);
 
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 app.listen(PORT, function () {
