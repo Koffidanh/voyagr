949deb0a5629c81e753cc8d4ae522de6c35db30b
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-27 21:00:06 -0600

    Commenting out components

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -17,9 +17,9 @@ function DashboardPage() {
     <div
       style={{ backgroundColor: "lightgray" }}
     >
-      <Header addPostLocation={addPostLocation} setAddPostLocation={setAddPostLocation} viewport={viewport} setViewport={setViewport} />
+      {/* <Header addPostLocation={addPostLocation} setAddPostLocation={setAddPostLocation} viewport={viewport} setViewport={setViewport} /> */}
       <Feed>
-        <MessageSender addPostLocation={addPostLocation} setAddPostLocation={setAddPostLocation} viewport={viewport} setViewport={setViewport} />
+        {/* <MessageSender addPostLocation={addPostLocation} setAddPostLocation={setAddPostLocation} viewport={viewport} setViewport={setViewport} /> */}
         <Post />
       </Feed>
     </div>
