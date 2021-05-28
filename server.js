e9962a11d9041693d09343219d6b57fcfef9e1ad
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-27 22:35:41 -0600

    Updated routes

--- server.js
+++ server.js
@@ -43,11 +43,11 @@ app.use(routes);
 // app.use(middlewares.errorHandler);
 
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+app.get("/dashboard", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function () {
